b83eef4fbf3d82ac1ab735cf1ca4dd8f87854297
wleng2001 <kamilgrodzki5@gmail.com>
2022-10-07 20:47:34 +0300

    Create PNG-Prime_Number_Generator.py

---
+++ PNG-Prime_Number_Generator.py
@@ -0,0 +1,48 @@
+#prime number generator
+from time import sleep
+from os import system
+from sys import platform
+
+examine_number=1
+if_prime_number=True
+file_name="prime_numbers.txt"
+
+def read_last_number(file_name):
+    count = -1
+    try:
+        for count, wiersz in enumerate(open(file_name, 'r')):
+            count += 1
+        file=open(file_name, 'r')
+        table=file.read()
+        number=(table.split('\n'))[-2]
+        return int(number)
+    except:
+        return 1
+
+def write_file(file_name, data):
+    file=open(file_name, 'a')
+    file.write(str(data)+'\n')
+    file.close()
+
+
+
+print("Read last prime number")
+examine_number=read_last_number(file_name)+1
+print(f"Last prime number is: {examine_number}")
+sleep(2)
+
+while True:
+    for j in range(2,int(examine_number/2+1)):
+        if examine_number%j==0:
+            if_prime_number=False
+            
+            break
+        
+    if if_prime_number==True:
+        print(f"Number {examine_number} is prime number")
+        write_file(file_name, examine_number)
+        #sleep(1.1)
+    examine_number+=1
+    if_prime_number=True
+
+
