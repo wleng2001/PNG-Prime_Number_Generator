836293c85c05dbd0c8377cc30f92c1801c569ea9
wleng2001 <kamilgrodzki5@gmail.com>
2022-10-20 00:20:52 +0300

    add update checker

--- PNG-Prime_Number_Generator.py
+++ PNG-Prime_Number_Generator.py
@@ -2,11 +2,28 @@
 from time import sleep
 from os import system
 from sys import platform
+from datetime import datetime
+from github import Github
 
 examine_number=1
 if_prime_number=True
 file_name="prime_numbers.txt"
+last_update="2022-10-19"
+repo_name="wleng2001/PNG-Prime_Number_Generator"
+#update check
 
+def update_check(last_update, repo_name):
+    last_date=datetime.fromisoformat(last_update)
+    g=Github()
+    for i in g.search_repositories("wleng2001/PNG-Prime_Number_Generator"):
+        repo=i
+    date=repo.pushed_at
+    if date.date()==last_date.date():
+        return False
+    else:
+        return True
+
+#prime number search
 def read_last_number(file_name):
     count = -1
     try:
@@ -25,6 +42,11 @@ def write_file(file_name, data):
     file.close()
 
 
+print("Check version...")
+if update_check(last_update, repo_name)==True:
+    print("New version is available!")
+else:
+    print("App is current")
 
 print("Read last prime number")
 examine_number=read_last_number(file_name)+1
